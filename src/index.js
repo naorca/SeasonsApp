312c8a857743d516d11d43c4be20898a6548697d
Naor Cohen Adiv <naorca@gmail.com>
2019-06-05 18:32:31 +0300

    First Commit

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
+
+class App extends React.Component {
+  state = { lat: null, errMessage: "" };
+
+  componentDidMount() {
+    // console.log("Component was rendered to the screen");
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errMessage: err.message })
+    );
+  }
+
+  render() {
+    if (this.state.errMessage && !this.state.lat) {
+      return <div>Error: {this.state.errMessage}</div>;
+    }
+
+    if (!this.state.errMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message={"Please accept location request"} />;
+  }
+}
+ReactDOM.render(<App />, document.querySelector("#root"));
